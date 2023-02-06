3f94490c250df4a148cbf302a9fc2db2453d9149
liemkg1234 <liemkg1234@gmail.com>
2023-02-06 19:07:46 +0700

    Update csv/excel file: Export file

--- split_entity.py
+++ split_entity.py
@@ -7,11 +7,11 @@ df.columns = df.columns.str.replace('Thunbnail', 'Thumbnail')
 
 # df.to_csv('product1.csv', sep='\t', encoding='utf-8')
 
-#						                               1-n	    n-n     n-n     n-n     n-n    n-n
+#						                               1-n	     n-n     n-n     n-n     n-n    n-n
 # Name, Price, Thumbnail, Description, Year, Quality, Category, Brand, Country, Format, Genre, Style
 
 
-# Product: Name, Price, Thumbnail, Description, Year, Quality
+# Product: Name, Price, Thumbnail, Description, Year, Quality, Category, Brand
 df_Product = df[['Name', 'Price', 'Thumbnail', 'Description', 'Year', 'Quality', 'Category', 'Brand']]
 # Replace "[;];'" -> null
 df_Product['Category'] = df_Product['Category'].str.replace('[', '')
@@ -29,38 +29,36 @@ df_Product['Brand'] = df_Product['Brand'].apply(lambda x: x.strip("*&%,"))
 df_Product.to_excel("product/product.xlsx", index=False, encoding='utf-8')
 
 # df_Product = df_Product.applymap(lambda x: '"' + str(x) + '"' if isinstance(x, (int, str)) else x)
-#
 # df_Product.to_csv('product/product.csv', sep='\t', encoding='utf-8', index=False)
 
+# Category        1-n
+df_Category = df[['Name', 'Category']]
+dffinal_Category = pd.DataFrame(columns=['Name', 'Category'])
+for index, row in df_Category.iterrows():
+    list_curr_Category = row['Category'].replace('[', '').replace(']', '').replace('', '').replace("'", '').split(',')
+    # print(list_curr_Category)
+    for Category in list_curr_Category:
+        # print(row['Name'], Category)
+        dffinal_Category = dffinal_Category.append({'Name': row['Name'], 'Category': Category}, ignore_index=True)
 
-# # Category        1-n
-# df_Category = df[['Name', 'Category']]
-# dffinal_Category = pd.DataFrame(columns=['Name', 'Category'])
-# for index, row in df_Category.iterrows():
-#     list_curr_Category = row['Category'].replace('[', '').replace(']', '').replace('', '').replace("'", '').split(',')
-#     # print(list_curr_Category)
-#     for Category in list_curr_Category:
-#         # print(row['Name'], Category)
-#         dffinal_Category = dffinal_Category.append({'Name': row['Name'], 'Category': Category}, ignore_index=True)
-#
-# dffinal_Category.to_csv('product/category.csv', sep='\t', encoding='utf-8', index=False)
-#
-#
-# # Brand          n-n
-# df_Brand = df[['Name', 'Brand']]
-# df_Brand = df_Brand.fillna('')
-# df_Product_Brand = pd.DataFrame(columns=['Name', 'Brand'])
-# for index, row in df_Brand.iterrows():
-#     list_curr_Brand = row['Brand'].split(',')
-#     for Brand in list_curr_Brand:
-#         df_Product_Brand = df_Product_Brand.append({'Name': row['Name'], 'Brand': Brand}, ignore_index=True)
-#
-# # Product_Brand
-# df_Product_Brand['Brand'] = df_Product_Brand['Brand'].str.strip()
-# df_Product_Brand.to_csv('product/product_brand.csv', sep='\t', encoding='utf-8', index=False)
-# # Brand
-# Brand_series = df_Product_Brand['Brand'].drop_duplicates()
-# Brand_series = Brand_series.replace({'': np.nan})
-# Brand_series = Brand_series.dropna()
-# Brand_series = Brand_series.reset_index(drop=True)
-# Brand_series.to_csv('product/brand.csv', sep='\t', encoding='utf-8', index=False)
+dffinal_Category.to_csv('product/category.csv', sep='\t', encoding='utf-8', index=False)
+
+
+# Brand          n-n
+df_Brand = df[['Name', 'Brand']]
+df_Brand = df_Brand.fillna('')
+df_Product_Brand = pd.DataFrame(columns=['Name', 'Brand'])
+for index, row in df_Brand.iterrows():
+    list_curr_Brand = row['Brand'].split(',')
+    for Brand in list_curr_Brand:
+        df_Product_Brand = df_Product_Brand.append({'Name': row['Name'], 'Brand': Brand}, ignore_index=True)
+
+# Product_Brand
+df_Product_Brand['Brand'] = df_Product_Brand['Brand'].str.strip()
+df_Product_Brand.to_csv('product/product_brand.csv', sep='\t', encoding='utf-8', index=False)
+# Brand
+Brand_series = df_Product_Brand['Brand'].drop_duplicates()
+Brand_series = Brand_series.replace({'': np.nan})
+Brand_series = Brand_series.dropna()
+Brand_series = Brand_series.reset_index(drop=True)
+Brand_series.to_csv('product/brand.csv', sep='\t', encoding='utf-8', index=False)
